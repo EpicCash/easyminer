7a1669bf8abe1e9bfa77397b9771f7a94b5a3197
blacktyger <blacktyg3r@gmail.com>
2022-10-25 22:08:04 +0100

    front-end works

--- frontend/static/scripts.js
+++ frontend/static/scripts.js
@@ -1,10 +1,10 @@
 // Running Loop keeping alive connection with back-end
 
-document.addEventListener('DOMContentLoaded', function () {
-    keep_alive_server()
-    try {setInterval(keep_alive_server, 5 * 1000)()
-    } catch (error) {}
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//     keep_alive_server()
+//     try {setInterval(keep_alive_server, 5 * 1000)()
+//     } catch (error) {}
+// });
 
 function keep_alive_server() {
     fetch(document.location + "keep-alive/?alive=true", {
@@ -24,19 +24,13 @@ function keep_alive_server() {
 
 
 function apiCall(body, query, cbFieldId, method='POST') {
-    fetch(query, {
+    return fetch(query, {
         method: method,
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(body),
-    })
-        .then(resp => resp.text())  // or, resp.json(), etc.
-        .then(data => {
-            document.getElementById(cbFieldId).innerHTML = data
-        })
-        .catch(error => {
-            console.error(error)
-        });
+    }).then(response => response.text())
+
 }
